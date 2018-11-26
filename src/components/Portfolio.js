2ea7c79132a5c73ba39312e6fbe87dc5a777d9fa
Thomas Erickson <terickson@spotx.tv>
2018-11-26 15:01:19 -0700

    put the json in place

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,7 +1,16 @@
 import React, {Component} from 'react';
-
+import movies from '../store/port-data';
 export default class Portfolio extends Component {
     render(){
+        const movied = movies.map((element) =>
+            <div className="col-4 col-6-medium col-12-small">
+                <article className="box style2">
+                    <a href={element.link} className="image featured"><img src={element.pic} alt="" width='348px' height='243px'  /></a>
+                    <h3><a href="#">{element.title}</a></h3>
+                    <p>{element.description}</p>
+                </article>
+            </div>
+        )
         return(
             <article id="portfolio" className="wrapper style3">
                 <div className="container">
@@ -9,54 +18,12 @@ export default class Portfolio extends Component {
                         <h2>Here’s some stuff I made recently.</h2>
                         <p>I was working on some pretty exciting Advante Garde projects before my untimely death</p>
                     </header>
+
                     <div className="row">
-
-                        <div className="col-4 col-6-medium col-12-small">
-                            <article className="box style2">
-                                <a href="#" className="image featured"><img src="images/pic01.jpg" alt="" /></a>
-                                <h3><a href="#">Magna feugiat</a></h3>
-                                <p>Ornare nulla proin odio consequat.</p>
-                            </article>
-                        </div>
-                        <div className="col-4 col-6-medium col-12-small">
-                            <article className="box style2">
-                                <a href="#" className="image featured"><img src="images/pic02.jpg" alt="" /></a>
-                                <h3><a href="#">Veroeros primis</a></h3>
-                                <p>Ornare nulla proin odio consequat.</p>
-                            </article>
-                        </div>
-                        <div className="col-4 col-6-medium col-12-small">
-                            <article className="box style2">
-                                <a href="#" className="image featured"><img src="images/pic03.jpg" alt="" /></a>
-                                <h3><a href="#">Lorem ipsum</a></h3>
-                                <p>Ornare nulla proin odio consequat.</p>
-                            </article>
-                        </div>
-                        {/* <div className="col-4 col-6-medium col-12-small">
-                            <article className="box style2">
-                                <a href="#" className="image featured"><img src="images/pic04.jpg" alt="" /></a>
-                                <h3><a href="#">Tempus dolore</a></h3>
-                                <p>Ornare nulla proin odio consequat.</p>
-                            </article>
-                        </div> */}
-
-                        <div className="col-4 col-6-medium col-12-small">
-                            <article className="box style2">
-                                <a href="#" className="image featured"><img src="images/pic05.jpg" alt="" /></a>
-                                <h3><a href="#">Feugiat aliquam</a></h3>
-                                <p>Ornare nulla proin odio consequat.</p>
-                            </article>
-                        </div>
-                        <div className="col-4 col-6-medium col-12-small">
-                            <article className="box style2">
-                                <a href="#" className="image featured"><img src="images/pic06.jpg" alt="" /></a>
-                                <h3><a href="#">Sed amet ornare</a></h3>
-                                <p>Ornare nulla proin odio consequat.</p>
-                            </article>
-                        </div>
+                        {movied}
                     </div>
                     <footer>
-                        <p>Lorem ipsum dolor sit sapien vestibulum ipsum primis?</p>
+                        <p>Just a small sampling of my crazy portfolio</p>
                         <a href="#contact" className="button large scrolly">Get in touch with me</a>
                     </footer>
                 </div>
