511c46c46bc6b39c77b9b0392f337f9298e23b84
Thomas Erickson <terickson@spotx.tv>
2018-11-28 12:43:47 -0700

    should be prod now

---
+++ build/precache-manifest.eb5f96e28ec04373f4e9408f2f388274.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0bb6fa0138ab92322ccd",
+    "url": "/static/js/main.0bb6fa01.chunk.js"
+  },
+  {
+    "revision": "c8aa14d3e74df53e9ced",
+    "url": "/static/js/1.c8aa14d3.chunk.js"
+  },
+  {
+    "revision": "0bb6fa0138ab92322ccd",
+    "url": "/static/css/main.acc73012.chunk.css"
+  },
+  {
+    "revision": "7b320526781ef45bb0c271389ca44c26",
+    "url": "/index.html"
+  }
+];
